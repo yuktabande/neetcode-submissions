b413055eb91554e42487d0cd3604013fc62da4ff
Yukta Bande <159476987+yuktabande@users.noreply.github.com>
2026-07-13 16:16:20 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.py
@@ -0,0 +1,13 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.data = []
+
+    def addNum(self, num: int) -> None:
+        self.data.append(num)
+
+    def findMedian(self) -> float:
+        self.data.sort()
+        n = len(self.data)
+        return (self.data[n // 2] if (n & 1) else
+                (self.data[n // 2] + self.data[n // 2 - 1]) / 2)
